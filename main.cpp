7cc25adbbeaa55b45413425451b7ad27f2d8fd39
La Minh Duc <laminhduc200@gmail.com>
2024-12-11 00:23:59 +0700

    Set check point for timing

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@
 int main()
 {
     RSA rsa;
-    rsa.setFilePlaintext("plaintext.txt");
-    rsa.setFilePublicKey("public.key");
-    rsa.setFilePrivateKey("private.key");
-    rsa.generateKeyPair(2048);
+    rsa.setFilePlaintext("test1.txt");
+    rsa.setFilePublicKey("public1.key");
+    rsa.setFilePrivateKey("private1.key");
+    auto now = std::chrono::high_resolution_clock::now();
+    rsa.generateKeyPair(512);
+    std::cout << "time to gen 512 bit key: " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - now).count() << " us" << std::endl;
+    now = std::chrono::high_resolution_clock::now();
     rsa.encrypt();
+    std::cout << "time to encrypt test1: " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - now).count() << " us" << std::endl;
     rsa.decrypt();
     return 0;
 }
